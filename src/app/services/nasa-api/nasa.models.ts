3ef404c263c9f7aa072ff3e7d6061b7544968b30
Joe Deger <degerjoe@gmail.com>
2022-09-25 16:36:54 -0600

    adding more api impls

--- src/app/services/nasa-api/nasa.models.ts
+++ src/app/services/nasa-api/nasa.models.ts
@@ -1,5 +1,16 @@
 export const API_KEY = 'tW6rCBJBYlF20CerfLdehN1ZWIq2Jvak7VgUzQ2I';
 
+export interface BaseParams {
+    api_key?: string;
+}
+
+export interface ApodParams extends BaseParams {
+    date?: string;
+    starte_date?: string;
+    end_date?: string;
+    count?: number;
+    thummbs?: boolean;
+}
 export interface ApodResp {
     date: string;
     explanation: string;
@@ -8,4 +19,141 @@ export interface ApodResp {
     service_version: string;
     title: string;
     url: string;
+}
+
+export interface NeoFeedParams extends BaseParams {
+    start_date?: string;
+    end_date?: string;
+}
+export interface NeoFeedResp {
+
+}
+
+export interface NeoLookupParams extends BaseParams {
+    asteroid_id?: number;
+}
+export interface NeoLookupResp {
+
+}
+
+export interface NeoBrowseResp {
+
+}
+
+export interface DonkiCmeParams extends BaseParams {
+    startDate?: string;
+    endDate?: string;
+}
+export interface DonkiCmeResp {
+
+}
+
+export interface DonkiCmeAnalysisParams {
+    startDate?: string;
+    endDate?: string;
+    mostAccurateOnly?: boolean;
+    completeEntryOnly?: boolean;
+    speed?: number;
+    halfAngle?: number;
+    catalog?: 'ALL' | 'SWRC_CATALOG' | 'JANG_ET_AL_CATALOG';
+    keyword?: string;
+}
+export interface DonkiCmeAnalysisResp {
+
+}
+
+export interface DonkiGstParams extends BaseParams {
+    startDate?: string;
+    endDate?: string;
+}
+export interface DonkiGstResp {
+
+}
+
+export interface DonkiIpsParams extends BaseParams {
+    startDate?: string;
+    endDate?: string;
+    location?: 'Earth' | 'MESSENGER' | 'STEREO A' | 'STEREO B';
+    catalog?: 'SWRC_CATALOG' | 'WINSLOW_MESSENGER_ICME_CATALOG';
+}
+export interface DonkiIpsResp {
+
+}
+
+export interface DonkiFlrParams extends BaseParams {
+    startDate?: string;
+    endDate?: string;
+}
+export interface DonkiFlrResp {
+
+}
+
+export interface DonkiSepParams extends BaseParams {
+    startDate?: string;
+    endDate?: string;
+}
+export interface DonkiSepResp {
+
+}
+
+export interface DonkiMpcParams extends BaseParams {
+    startDate?: string;
+    endDate?: string;
+}
+export interface DonkiMpcResp {
+
+}
+
+export interface DonkiRbeParams extends BaseParams {
+    startDate?: string;
+    endDate?: string;    
+}
+export interface DonkiRbeResp {
+
+}
+
+export interface DonkiHssParams extends BaseParams {
+    startDate?: string;
+    endDate?: string;    
+}
+export interface DonkiHssResp {
+
+}
+
+export interface DonkiWsaParams extends BaseParams {
+    startDate?: string;
+    endDate?: string;    
+}
+export interface DonkiWsaResp {
+
+}
+
+export interface DonkiNotificationsParams extends BaseParams {
+    startDate?: string;
+    endDate?: string;
+    type?: 'all' | 'FLR' | 'SEP' | 'CME' | 'IPS' | 'MPC' | 'GST' | 'RBE' | 'report';
+}
+export interface DonkiNotificationsResp {
+
+}
+
+export interface EarthImageryParams extends BaseParams {
+    lat?: number;
+    lon?: number;
+    dim?: number;
+    date?: string;
+    cloud_score?: boolean;
+}
+export interface EarthImageryResp {
+
+}
+
+export interface EarthAssetsParams extends BaseParams {
+    lat?: number;
+    lon?: number;
+    date?: string;
+    dim?: number;
+}
+export interface EarthAssetsResp {
+
 }
--- src/app/services/nasa-api/nasa.service.ts
+++ src/app/services/nasa-api/nasa.service.ts
@@ -1,23 +1,121 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from '@angular/common/http';
-import { API_KEY, ApodResp } from "./nasa.models";
+import { API_KEY, ApodParams, ApodResp, BaseParams, DonkiCmeAnalysisResp, DonkiCmeResp, DonkiFlrResp, DonkiGstResp, DonkiHssResp, DonkiIpsResp, DonkiMpcResp, DonkiNotificationsResp, DonkiRbeResp, DonkiSepResp, DonkiWsaResp, EarthAssetsResp, EarthImageryResp, NeoBrowseResp, NeoFeedParams, NeoFeedResp, NeoLookupResp } from "./nasa.models";
 import { Observable } from "rxjs";
 
 @Injectable({
     providedIn: 'root'
 })
 export class NasaService {
-    private baseUrl = 'https://api.nasa.gov';
-    private params = {
+    private readonly baseUrl = 'https://api.nasa.gov';
+    private readonly baseParams: BaseParams = {
         'api_key': API_KEY,
     }
-    private apodUrl = `${this.baseUrl}/planetary/apod`;
+
+    private readonly apodUrl = `${this.baseUrl}/planetary/apod`;
+
+    private readonly neoFeedUrl = `${this.baseUrl}/neo/rest/v1/feed`;
+    private readonly neoLookupUrl = `${this.baseUrl}/neo/rest/v1/neo`;
+    private readonly neoBrowseUrl = `${this.baseUrl}/neo/rest/v1/neo/browse`;
+
+    private readonly donkiCmeUrl = `${this.baseUrl}/DONKI/CME`;
+    private readonly donkiCmeAnalysisUrl = `${this.baseUrl}/DONKI/CMEAnalysis`;
+    private readonly donkiGstUrl = `${this.baseUrl}/DONKI/GST`;
+    private readonly donkiIpsUrl = `${this.baseUrl}/DONKI/IPS`;
+    private readonly donkiFlrUrl = `${this.baseUrl}/DONKI/FLR`;
+    private readonly donkiSepUrl = `${this.baseUrl}/DONKI/SEP`;
+    private readonly donkiMpcUrl = `${this.baseUrl}/DONKI/MPC`;
+    private readonly donkiRbeUrl = `${this.baseUrl}/DONKI/RBE`;
+    private readonly donkiHssUrl = `${this.baseUrl}/DONKI/HSS`;
+    private readonly donkiWsaUrl = `${this.baseUrl}/DONKI/WSAEnlilSimulations`;
+    private readonly donkiNotificationsUrl = `${this.baseUrl}/DONKI/notifications`;
+
+    private readonly earthImageryUrl = `${this.baseUrl}/planetary/earth/imagery`;
+    private readonly earthAssetsUrl = `${this.baseUrl}/planetary/earth/assets`;
 
     constructor(
         private readonly http: HttpClient,
     ) {}
 
-    public apod(): Observable<ApodResp> {
-        return this.http.get<ApodResp>(`${this.apodUrl}`, { 'params': this.params });
+    // APOD
+    public apod(inputParams?: ApodParams): Observable<ApodResp> {
+        return this.http.get<ApodResp>(`${this.apodUrl}`, {
+            'params': {
+                ...this.baseParams,
+                ...inputParams,
+            }
+        });
+    }
+
+    // NEO
+    public neoFeed(inputParams?: NeoFeedParams): Observable<NeoFeedResp> {
+        return this.http.get<NeoFeedResp>(`${this.neoFeedUrl}`, {
+            'params' : {
+                ...this.baseParams,
+                ...inputParams,
+            }
+        });
+    }
+
+    public neoLookup(): Observable<NeoLookupResp> {
+        return this.http.get<NeoLookupResp>(`${this.neoLookupUrl}`, {});
+    }
+
+    public neoBrowse(): Observable<NeoBrowseResp> {
+        return this.http.get<NeoBrowseResp>(`${this.neoBrowseUrl}`, {});
+    }
+
+    // DONKI
+    public donkiCme(): Observable<DonkiCmeResp> {
+        return this.http.get<DonkiCmeResp>(`${this.donkiCmeUrl}`, {});
+    }
+
+    public donkiCmeAnalysis(): Observable<DonkiCmeAnalysisResp> {
+        return this.http.get<DonkiCmeAnalysisResp>(`${this.donkiCmeAnalysisUrl}`, {});
+    }
+
+    public donkiGst(): Observable<DonkiGstResp> {
+        return this.http.get<DonkiGstResp>(`${this.donkiGstUrl}`, {});
+    }
+
+    public donkiIps(): Observable<DonkiIpsResp> {
+        return this.http.get<DonkiIpsResp>(`${this.donkiIpsUrl}`, {});
+    }
+
+    public donkiFlr(): Observable<DonkiFlrResp> {
+        return this.http.get<DonkiFlrResp>(`${this.donkiFlrUrl}`, {});
+    }
+
+    public donkiSep(): Observable<DonkiSepResp> {
+        return this.http.get<DonkiSepResp>(`${this.donkiSepUrl}`, {});
+    }
+
+    public donkiMpc(): Observable<DonkiMpcResp> {
+        return this.http.get<DonkiMpcResp>(`${this.donkiMpcUrl}`, {});
+    }
+
+    public donkiRbe(): Observable<DonkiRbeResp> {
+        return this.http.get<DonkiRbeResp>(`${this.donkiRbeUrl}`, {});
+    }
+
+    public donkiHss(): Observable<DonkiHssResp> {
+        return this.http.get<DonkiHssResp>(`${this.donkiHssUrl}`, {});
+    }
+
+    public donkiWsa(): Observable<DonkiWsaResp> {
+        return this.http.get<DonkiWsaResp>(`${this.donkiWsaUrl}`, {});
+    }
+
+    public donkiNotifications(): Observable<DonkiNotificationsResp> {
+        return this.http.get<DonkiNotificationsResp>(`${this.donkiNotificationsUrl}`, {});
+    }
+
+    // EARTH
+    public earthImagery(): Observable<EarthImageryResp> {
+        return this.http.get<EarthImageryResp>(`${this.earthImageryUrl}`, {});
+    }
+
+    public earthAssets(): Observable<EarthAssetsResp> {
+        return this.http.get<EarthAssetsResp>(`${this.earthAssetsUrl}`, {});
     }
 }
