5b7e478059d930550b9eb1b0643d3952a58b0e78
Joe Deger <degerjoe@gmail.com>
2022-09-29 20:31:04 -0600

    neo lookup resp type

--- src/app/services/nasa-api/neo/neo.models.ts
+++ src/app/services/nasa-api/neo/neo.models.ts
@@ -74,8 +74,41 @@ export interface EstimatedDiameterData {
 }
 
 // Lookup
-export interface NeoLookupResp {
+export interface NeoLookupResp extends NeoResp {
+    designator: string;
+    orbital_data: NepOrbitalData;
+}
 
+export interface NepOrbitalData {
+    aphelion_distance: string;
+    ascending_node_longitude: string;
+    data_arc_in_days: number;
+    eccentricity: string;
+    epoch_osculation: string;
+    equinox: string;
+    first_observation_date: string;
+    inclination: string;
+    jupiter_tisserand_invariant: string;
+    last_observation_date: string;
+    mean_anomaly: string;
+    mean_motion: string;
+    minimum_orbit_intersection: string;
+    observations_used: number;
+    orbit_class: OrbitClass;
+    orbit_determination_date: string;
+    orbit_id: string;
+    orbit_uncertainty: string;
+    orbital_period: string;
+    perihelion_argument: string;
+    perihelion_distance: string;
+    perihelion_time: string;
+    semi_major_axis: string;
+}
+
+export interface OrbitClass {
+    orbit_class_description: string;
+    orbit_class_range: string;
+    orbit_class_type: string;
 }
 
 // Browse
